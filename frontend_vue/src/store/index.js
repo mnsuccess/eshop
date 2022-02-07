c68a09d36c9315fd3c81a32fa438ee023456b037
mnsuccess <ngudiamanasse@gmail.com>
2022-02-07 11:22:29 +0200

    [NEW][F] Create and register Vuex Transaction store module for handling Transactions features (listing Transactions,purchse product and TopUp wallet)

--- frontend_vue/src/store/index.js
+++ frontend_vue/src/store/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 import * as user from "@/store/modules/User";
 import * as product from "@/store/modules/Product";
+import * as transaction from "@/store/modules/Transaction";
 
 Vue.use(Vuex);
 
@@ -12,5 +13,6 @@ export default new Vuex.Store({
   modules: {
     user,
     product,
+    transaction,
   },
 });
