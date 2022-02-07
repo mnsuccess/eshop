85e9dab612c04bdf89b001b5965ebb17176a9339
mnsuccess <ngudiamanasse@gmail.com>
2022-02-07 12:02:04 +0200

    [NEW][F] Create TopUp Component for handling Topup amount in the user wallet

---
+++ frontend_vue/src/components/TopuUp.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="max-w-md bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
+  >
+    <div class="flex flex-col items-center p-20 pb-10 mt-2">
+      <div class="relative mb-2">
+        <input
+          v-model="amount"
+          name="amount"
+          type="numeric"
+          id="amount"
+          placeholder="Amount"
+          autofocus
+          class="w-full px-3 pt-5 pb-2 border border-gray-300 rounded focus:border-blue-700 focus:ring-1 focus:ring-blue-700 focus:outline-none input active:outline-none"
+        />
+      </div>
+      <div class="flex justify-center mt-4 lg:mt-6">
+        <a
+          @click="topup"
+          class="inline-flex items-center px-12 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >TopUp</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "TopUp",
+  data() {
+    return {
+      amount: 0,
+    };
+  },
+  computed: {
+    ...mapGetters("transaction", ["loading", "error"]),
+  },
+  methods: {
+    topup() {
+      this.$store.dispatch("transaction/topupWallet", this.amount);
+      this.amount = "";
+    },
+  },
+};
+</script>
