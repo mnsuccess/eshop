0552e6b63c6abb7331880a054e8fe36e59c3e78c
mnsuccess <ngudiamanasse@gmail.com>
2022-02-07 11:57:06 +0200

    [NEW][f] create ProductCard and FlashMessage components | implememts Products  and Product view

--- frontend_vue/src/views/Product.vue
+++ frontend_vue/src/views/Product.vue
@@ -1,10 +1,57 @@
 <template>
-  <div class="">
-    <h1>This is A Product view page</h1>
+  <div class="container px-6 mx-auto sm:mt-32">
+    <FlashMessage message="loading..." v-if="loading" key="loading" />
+    <div v-if="error">
+      <h3 class="text-2xl font-medium text-gray-700">
+        ⚠️ No Product Found | Check you Api Connection
+      </h3>
+      <transition name="fade">
+        <FlashMessage :error="error" v-if="error" key="error" />
+      </transition>
+    </div>
+    <div v-if="product" class="md:flex md:items-center">
+      <div class="w-full h-64 md:w-1/2 lg:h-96">
+        <img
+          class="object-cover w-full h-full max-w-lg mx-auto rounded-md"
+          :src="`${product.image}`"
+          alt="image"
+        />
+      </div>
+      <div class="w-full max-w-lg mx-auto mt-5 md:ml-8 md:mt-0 md:w-1/2">
+        <h3 class="text-lg text-gray-700 uppercase">{{ product.name }}</h3>
+        <span class="mt-3 text-gray-500">${{ product.price }}</span>
+        <span class="mt-2 text-gray-500" v-if="product.discount">
+          | Discount {{ product.discount }}%</span
+        >
+        <span class="mt-2 text-gray-500" v-if="product.discount">
+          | ${{ product.discounted_price }}</span
+        >
+        <hr class="my-3" />
+        <div class="mt-2">
+          <label class="text-sm text-gray-700" for="count">Description</label>
+          <p>{{ product.description }}</p>
+        </div>
+
+        <div class="flex items-center justify-center mt-6">
+          <Purchase :product_id="product.id" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import { mapGetters } from "vuex";
+import FlashMessage from "@/components/FlashMessage";
+import Purchase from "@/components/Purchase";
 export default {
   name: "Product",
+  props: ["id"],
+  components: { FlashMessage, Purchase },
+  computed: {
+    ...mapGetters("product", ["loading", "error", "product"]),
+  },
+  created() {
+    this.$store.dispatch("product/getProduct", this.id);
+  },
 };
 </script>
--- frontend_vue/src/views/Products.vue
+++ frontend_vue/src/views/Products.vue
@@ -1,10 +1,49 @@
 <template>
-  <div class="">
-    <h1>This is A Products listing page</h1>
+  <div class="container px-6 mx-auto">
+    <FlashMessage
+      message="loading..."
+      v-if="loading && !totalProducts"
+      key="loading"
+    />
+    <h3 v-if="totalProducts > 1" class="text-2xl font-medium text-gray-700">
+      Our Best Product
+    </h3>
+    <span v-if="totalProducts > 1" class="mt-3 text-sm text-gray-500"
+      >{{ totalProducts }}+ Products</span
+    >
+    <div v-if="totalProducts == 0">
+      <h3 class="text-2xl font-medium text-gray-700">
+        ⚠️ No Products available | Check you Api Connection
+      </h3>
+      <transition name="fade">
+        <FlashMessage :error="error" v-if="error" key="error" />
+      </transition>
+    </div>
+
+    <div
+      class="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+    >
+      <ProductCard
+        v-for="product in products"
+        :key="product['@id']"
+        :product="product"
+      />
+    </div>
   </div>
 </template>
+
 <script>
+import { mapGetters } from "vuex";
+import ProductCard from "@/components/ProductCard";
+import FlashMessage from "@/components/FlashMessage";
 export default {
   name: "Products",
+  components: { ProductCard, FlashMessage },
+  computed: {
+    ...mapGetters("product", ["loading", "error", "products", "totalProducts"]),
+  },
+  created() {
+    this.$store.dispatch("product/getProducts");
+  },
 };
 </script>
